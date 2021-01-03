ea65c3bbb76bf677c402b26c4b0a877ce8cf523a
Prathiksha2000 <75937462+Prathiksha2000@users.noreply.github.com>
2021-01-03 16:56:41 +0530

    linked_list solutions

---
+++ linked_list/Middle of the Linked List.cpp
@@ -0,0 +1,43 @@
+/*
+Given a non-empty, singly linked list with head node head, return a middle node of linked list.
+If there are two middle nodes, return the second middle node.
+Example 1:
+Input: [1,2,3,4,5]
+Output: Node 3 from this list (Serialization: [3,4,5])
+The returned node has value 3.  (The judge's serialization of this node is [3,4,5]).
+Note that we returned a ListNode object ans, such that:
+ans.val = 3, ans.next.val = 4, ans.next.next.val = 5, and ans.next.next.next = NULL.
+*/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+ 
+ 
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+    ListNode* slow_ptr = head;  
+    ListNode* fast_ptr = head;  
+  
+    if (head!=NULL)  
+    {  
+        while (fast_ptr != NULL && fast_ptr->next != NULL)  
+        {  
+            fast_ptr = fast_ptr->next->next;  
+            slow_ptr = slow_ptr->next;  
+        }  
+       
+    }  
+        // cout << slow_ptr->val;  is the middle element
+     return slow_ptr;
+        
+}
+};
