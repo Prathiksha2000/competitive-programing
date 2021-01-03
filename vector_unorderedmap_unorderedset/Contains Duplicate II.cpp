52e172b15dc9af26281bdd93cef794b732b88378
Prathiksha2000 <75937462+Prathiksha2000@users.noreply.github.com>
2021-01-03 15:38:31 +0530

    vector_unorderedmap_unorderedset solutions

---
+++ vector_unorderedmap_unorderedset/Contains Duplicate II.cpp
@@ -0,0 +1,24 @@
+//Given an array of integers and an integer k, find out whether there are two distinct indices i and j in the array such that nums[i] = nums[j] 
+//and the absolute difference between i and j is at most k.
+//Example 1:
+//Input: nums = [1,2,3,1], k = 3
+//Output: true
+
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+class Solution {
+public:
+   bool containsNearbyDuplicate(vector<int>& nums, int k) {
+    unordered_map<int,int>data;
+    for(int i=0;i<nums.size();i++){
+        if(data.find(nums[i])!=data.end()){
+            int old_index = data.find(nums[i])->second;
+            if(i-old_index<=k) return true;
+        }
+        data[nums[i]] = i;
+    }
+    return false;
+    }
+};
